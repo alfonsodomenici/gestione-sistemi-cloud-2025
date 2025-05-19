1bcade2f892a9341facd5de25e0556e87a004e37
alfonso.domenici@gmail.com <alfonso.domenici@gmail.com>
2025-05-19 16:57:44 +0200

    Enhance multiplication form: add reset button, improve validation, and style adjustments

--- 04-js/main.js
+++ 04-js/main.js
@@ -6,16 +6,29 @@ const n1 = document.querySelector("#n1");
 
 const n2 = document.querySelector("#n2");
 
-const btn = document.querySelector("button");
+const btn = document.querySelector("button[type='submit']");
+
+const btnReset = document.querySelector("button[type='button']");
 
 const result = document.querySelector("#result");
 
+const form = document.querySelector("form");
 
-const onBtnClick = () => {
-    console.log("click");
+const onBtnClick = (e) => {
+    e.preventDefault(); //evita il refresh della pagina
+    if(form.checkValidity() === false) {
+        form.reportValidity();
+        return;
+    }   
     const risultato = prodotto(parseInt(n1.value),
         parseInt(n2.value));
     result.textContent = risultato;
 }
 
+const onBtnResetClick = (e) => {
+    form.reset();
+    result.textContent = "";
+}
+
+btnReset.addEventListener("click", onBtnResetClick);
 btn.addEventListener("click", onBtnClick);
