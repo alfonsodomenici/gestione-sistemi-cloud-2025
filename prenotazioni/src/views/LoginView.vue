30e1d2277f31b2a03d342ee0313631e484585663
alfonso.domenici@gmail.com <alfonso.domenici@gmail.com>
2025-06-03 13:53:23 +0200

    Refactor login and registration forms: improve structure and styling

--- prenotazioni/src/views/LoginView.vue
+++ prenotazioni/src/views/LoginView.vue
@@ -1,27 +1,29 @@
 <script setup>
 
-const onLogin = (e) =>{
+const onLogin = (e) => {
     console.log('effettuare login');
 }
 </script>
 
 <template>
-    <form class="box">
-        <p class="title has-text-centered">Login</p>
-        <div class="field">
-            <label class="label">Email</label>
-            <div class="control">
-                <input class="input" type="email" name="email" id="email" required>
+    <div class="columns is-centered mt-5">
+        <form class="box column is-half" @submit.prevent>
+            <p class="title has-text-centered">Login</p>
+            <div class="field">
+                <label class="label">Email</label>
+                <div class="control">
+                    <input class="input" type="email" name="email" id="email" required>
+                </div>
             </div>
-        </div>
-        <div class="field">
-            <label class="label">Password</label>
-            <div class="control">
-                <input class="input" type="password" name="pwd" id="pwd" required>
+            <div class="field">
+                <label class="label">Password</label>
+                <div class="control">
+                    <input class="input" type="password" name="pwd" id="pwd" required>
+                </div>
             </div>
-        </div>
-        <div class="field">
-            <button @click="onLogin" class="button is-primary" >Login</button>
-        </div>
-    </form>
+            <div class="field">
+                <button @click="onLogin" class="button is-primary">Login</button>
+            </div>
+        </form>
+    </div>
 </template>
--- prenotazioni/src/views/RegistrationView.vue
+++ prenotazioni/src/views/RegistrationView.vue
@@ -1,45 +1,47 @@
 <script setup>
 
-const onRegistration = (e)=>{
+const onRegistration = (e) => {
     console.log('effettuare registrazione')
 }
 </script>
 
 <template>
-    <form class="box">
-        <p class="title has-text-centered">Registrati</p>
-        <div class="field">
-            <label class="label">Nome</label>
-            <div class="control">
-                <input class="input" type="text" name="nome" id="nome" required>
+    <div class="columns is-centered mt-5">
+        <form class="box column is-half">
+            <p class="title has-text-centered">Registrati</p>
+            <div class="field">
+                <label class="label">Nome</label>
+                <div class="control">
+                    <input class="input" type="text" name="nome" id="nome" required>
+                </div>
             </div>
-        </div>
-        <div class="field">
-            <label class="label">Cognome</label>
-            <div class="control">
-                <input class="input" type="text" name="cognome" id="cognome" required>
+            <div class="field">
+                <label class="label">Cognome</label>
+                <div class="control">
+                    <input class="input" type="text" name="cognome" id="cognome" required>
+                </div>
             </div>
-        </div>
-        <div class="field">
-            <label class="label">Tel</label>
-            <div class="control">
-                <input class="input" type="tel" name="tel" id="tel" required>
+            <div class="field">
+                <label class="label">Tel</label>
+                <div class="control">
+                    <input class="input" type="tel" name="tel" id="tel" required>
+                </div>
             </div>
-        </div>
-        <div class="field">
-            <label class="label">Email</label>
-            <div class="control">
-                <input class="input" type="email" name="email" id="email" required>
+            <div class="field">
+                <label class="label">Email</label>
+                <div class="control">
+                    <input class="input" type="email" name="email" id="email" required>
+                </div>
             </div>
-        </div>
-        <div class="field">
-            <label class="label">Password</label>
-            <div class="control">
-                <input class="input" type="password" name="pwd" id="pwd" required>
+            <div class="field">
+                <label class="label">Password</label>
+                <div class="control">
+                    <input class="input" type="password" name="pwd" id="pwd" required>
+                </div>
             </div>
-        </div>
-        <div class="field">
-            <button @click="onRegistration" class="button is-primary">Registrati</button>
-        </div>
-    </form>
+            <div class="field">
+                <button @click="onRegistration" class="button is-primary">Registrati</button>
+            </div>
+        </form>
+    </div>
 </template>
