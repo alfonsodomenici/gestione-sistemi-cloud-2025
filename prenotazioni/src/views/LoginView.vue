0bd0837f34dc0f65212da79fa580b6b268480d6d
alfonso.domenici@gmail.com <alfonso.domenici@gmail.com>
2025-06-03 14:56:58 +0200

    Refactor login form: improve validation handling and bind input fields to credential model

--- prenotazioni/src/views/LoginView.vue
+++ prenotazioni/src/views/LoginView.vue
@@ -1,28 +1,43 @@
 <script setup>
+import { ref } from 'vue';
+
+const formLogin = ref(null);
+const credential = ref({
+    mail: '',
+    pwd: ''
+});
+
 
 const onLogin = (e) => {
-    console.log('effettuare login');
+    console.log(formLogin.value.checkValidity())
+    if(formLogin.value.checkValidity() === false) {
+        formLogin.value.reportValidity();
+        console.log('Form login is invalid');
+        return;
+    }
+    console.log('Login attempt with:', 
+        credential.value.mail, credential.value.pwd);
 }
 </script>
 
 <template>
     <div class="columns is-centered mt-5">
-        <form class="box column is-half" @submit.prevent>
+        <form ref="formLogin" class="box column is-half">
             <p class="title has-text-centered">Login</p>
             <div class="field">
                 <label class="label">Email</label>
                 <div class="control">
-                    <input class="input" type="email" name="email" id="email" required>
+                    <input v-model="credential.mail" class="input" type="email" name="email" id="email" required>
                 </div>
             </div>
             <div class="field">
                 <label class="label">Password</label>
                 <div class="control">
-                    <input class="input" type="password" name="pwd" id="pwd" required>
+                    <input v-model="credential.pwd" class="input" type="password" name="pwd" id="pwd" required>
                 </div>
             </div>
             <div class="field">
-                <button @click="onLogin" class="button is-primary">Login</button>
+                <button @click.prevent="onLogin" class="button is-primary">Login</button>
             </div>
         </form>
     </div>
